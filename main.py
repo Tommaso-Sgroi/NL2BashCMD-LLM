51fcbf1aadafb30c110fd437289037799b762172
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2024-08-25 02:45:44 +0200

    little setup

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ maybe use https://github.com/abetlen/llama-cpp-python to use a very fast version
 """
 import math
 from json import dumps
+
+import tqdm
+
 from neurips2020utils.metric.metric_utils import compute_metric
 from utils.send_request import *
 from utils.data_loader import get_dataset
@@ -56,13 +59,13 @@ def tellina_inference(model_name, notes=''):
     with open(f'./benchmarks/{model_name}-{notes}-.tmp.json', 'a') as benchtmp:
         benchtmp.write('{')
         completition, total_entries = 0, len(dataset_tellina)
-        for k, command in dataset_tellina.items():
+        for k, command in tqdm.tqdm(dataset_tellina.items()):
             k = int(k)
 
-            nl = command['invocation']
+            nl = f'{command["invocation"]}. Bash command: '
             gtcmd = command['cmd']
 
-            print(f'processing {k}, completition {(completition/total_entries)*100:.4f}%')
+            # print(f'processing {k}, completition {(completition/total_entries)*100:.4f}%')
 
             prediction, confidence = inference(nl)
             score = compute_metric(predicted_cmd=prediction, predicted_confidence=confidence, ground_truth_cmd=gtcmd)
@@ -84,7 +87,7 @@ def tellina_inference(model_name, notes=''):
 
 
 if __name__ == '__main__':
-    total = tellina_inference('Meta-Llama-3-8B.Q8_0.gguf', notes='NO_prompt_engineering')
+    total = tellina_inference('Meta-Llama-3-8B.Q8_0.gguf', notes='WITH_prompt_engineering')
     print('total accuracy', total)
 
 # gtnl = 'Set permission of "file" to read only for the owner'
