9fa3081671826d69420e651813482bcde2949bab
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2024-08-26 19:24:28 +0200

    Minor fixes

--- together_api.py
+++ together_api.py
@@ -12,12 +12,12 @@ from neurips2020utils.metric.metric_utils import compute_metric
 from utils.data_loader import get_dataset
 from utils.send_request import RequestData, inference_with_together_api, wait_rate_limit, inference_with_api
 
-rate_limit = 1 # one request for second :(
-use_proxy = False
-proxies = None
-dataset_path = ""
-dataset = None
-url = ""
+# rate_limit = 1 # one request for second :(
+# use_proxy = False
+# proxies = None
+# dataset_path = ""
+dataset = {}
+# url = ""
 
 
 headers = {
@@ -33,7 +33,6 @@ data = {
     "seed": 438441351351443,
     "stop": ['<|im_end|>', '</s>', '\n'],
 }
-
 
 def inference(prompt):
     data['prompt'] = prompt
@@ -137,7 +136,8 @@ def benchmark(model_name, notes='', base_prompt=''):
             benchtmp.write(f'"{k}": {dumps(predictions[k])},\n')
             # time_passed = time.time() - reset_limit
             # wait_rate_limit(rate_limit - time_passed)
-            wait_rate_limit(rate_limit)
+            if rate_limit >= 0:
+                wait_rate_limit(rate_limit)
 
         with open(outfile, 'w') as benchmark_tellina:
             dump(predictions, benchmark_tellina, indent=2)
@@ -153,6 +153,41 @@ def total_accuracy(path):
         total_score += v['score']
     return total_score / len(data)
 
+
+def setup_envs():
+    # -------------------------------------------
+    # scary area, pythonic jump scare!!!
+    args = argparser.parser.parse_args()
+    for arg in vars(args):
+        var = str(arg).upper()
+        value = str(getattr(args, str(arg)))
+        if var != '' and  value != '':
+            os.environ[var] = value # override env variables
+
+    # beware, war crimes ahead
+    # for arg in vars(args):
+    #     os.environ[str(arg).upper()] = (
+    #         os.environ)[str(arg).upper()] \
+    #         if os.environ[str(arg).upper()] == '' \
+    #         else str(getattr(args, str(arg))
+    #     ) # override env variables
+    # -------------------------------------------
+
+
+setup_envs()
+rate_limit = float(os.getenv('RATE_LIMIT'))  # one request for second :(
+use_proxy = os.getenv('PROXY_PIA') if os.getenv('PROXY_PIA') != '' else False
+dataset_path = os.getenv('DATASET_PATH')
+url = os.getenv('URL')
+model = os.getenv('MODEL_PATH')
+notes = os.getenv('NOTES')
+proxies = None if not use_proxy else {'all': os.getenv('PROXY_PIA')}
+data['temperature'] = float(os.getenv('TEMPERATURE'))
+headers['Authorization'] = f"Bearer {os.getenv('API_KEY')}"
+
+
+
+
 if __name__ == '__main__':
 
     # with open('benchmarks/tmp.[Llama-3-8b-hf]-nl2bash-data.json.json', 'w' ) as f:
@@ -162,7 +197,6 @@ if __name__ == '__main__':
     #             f.write(f"{k}: {v},\n")
     #
     #
-    # quit()
     base_prompt = '''Task: Convert the following descriptions into bash commands.
     Description: Remove all files in the current directory.
     Bash Command: rm -rf *
@@ -189,39 +223,12 @@ if __name__ == '__main__':
     Bash Command: ls -a | grep .txt
 
 '''
-    # -------------------------------------------
-    # scary area, pythonic jump scare!!!
-    args = argparser.parser.parse_args()
-    for arg in vars(args):
-        var = str(arg).upper()
-        value = str(getattr(args, str(arg)))
-        if var != '' and  value != '':
-            os.environ[var] = value # override env variables
-
-    # beware, war crimes ahead
-    # for arg in vars(args):
-    #     os.environ[str(arg).upper()] = (
-    #         os.environ)[str(arg).upper()] \
-    #         if os.environ[str(arg).upper()] == '' \
-    #         else str(getattr(args, str(arg))
-    #     ) # override env variables
-    # -------------------------------------------
-
-    rate_limit = float(os.getenv('RATE_LIMIT'))  # one request for second :(
-    use_proxy = os.getenv('PROXY_PIA') if os.getenv('PROXY_PIA') != '' else False
-    dataset_path = os.getenv('DATASET_PATH')
-    url = os.getenv('URL')
-    model = os.getenv('MODEL_PATH')
-    notes = os.getenv('NOTES')
-    proxies = None if not use_proxy else {'https': os.getenv('PROXY_PIA')}
-    data['temperature'] = float(os.getenv('TEMPERATURE'))
-    headers['Authorization'] = f"Bearer {os.getenv('TOGETHER_API_KEY')}"
-
-    tmp = {'rate':rate_limit, 'proxy':use_proxy, 'dataset_path':dataset_path, 'url':url, 'model':model}
-    if '' in tmp:
-        raise Error(f"some variable missing: {tmp}")
 
     dataset = get_dataset(dataset_path)
+    required = {'rate': rate_limit, 'proxy': use_proxy, 'dataset_path': dataset_path, 'url': url, 'model': model}
+    if '' in required:
+        raise Error(f"some variable missing: {required}")
+    del required
 
     total = benchmark(model, notes=notes, base_prompt=base_prompt)
     # total = benchmark(model, notes=notes) # uncomment to use it without custom prompt
@@ -233,3 +240,4 @@ if __name__ == '__main__':
 
     # print('total accuracy', avg)
 
+    # ping proxy-nl.privateinternetaccess.com
